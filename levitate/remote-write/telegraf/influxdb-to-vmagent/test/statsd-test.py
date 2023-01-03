e3a7bc6f360f372dacce32d642767a831a27b8a8
Saurabh Hirani <saurabh.hirani@gmail.com>
2023-01-03 12:23:02 +0530

    adding remote-write/telegraf/influxdb-to-vmagent

---
+++ levitate/remote-write/telegraf/influxdb-to-vmagent/test/statsd-test.py
@@ -0,0 +1,12 @@
+import statsd
+from time import sleep
+from random import randint, choice
+
+c = statsd.StatsClient('localhost', 8125, prefix='performance')
+
+while True:
+    incr = randint(1, 5)
+    metric_type = choice(['A', 'B', 'C'])
+    print(f"\radding metric: type: {metric_type}, incr: {incr}", end="")
+    c.incr(f'request.successful.count,type={metric_type}', incr)
+    sleep(1)
